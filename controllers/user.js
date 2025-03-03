0025ecf0fe726ff07e1c0a720964743d939acac5
Drew-A <drew.a.anzalone@gmail.com>
2025-03-03 11:55:34 -0800

    combined user and public user into 1 file, removed /posts/ routes

--- controllers/user.js
+++ controllers/user.js
@@ -1,72 +1,67 @@
 const express = require('express');
-const isSignedIn = require("./middleware/is-signed-in.js")
+const isSignedIn = require("../middleware/is-signed-in.js")
 const router = express.Router();
 const processTags = (req) => req.body.tags.split(",").map(tag => tag.trim().toLowerCase()).filter(e => e);
 
 const User = require('../models/user.js');
 const Image = require('../models/image.js');
 
-function formatDate(date) {
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-  const hours = String(date.getHours()).padStart(2, '0');
-  const minutes = String(date.getMinutes()).padStart(2, '0');
-  const seconds = String(date.getSeconds()).padStart(2, '0');
-
-  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
-}
-
 //! /users
+//? GET routes
 router.get('/:username/', async (req, res) => {
-  const userImages = await Image.find({ uploader: uid });
-  const user = User.find({ username: req.params.username });
+  const uploader = req.params.username;
+  const userImages = await Image.find({ uploader });
+
+  const verified = req.session.user?.username === uploader;
+  const username = verified ? "My" : uploader + "'s";
   const images = userImages.toReversed();
-  res.render('posts/index.ejs', { images, username: req.params.username+"'s", verified: false });
-  
-  user.then((result) => {
-    const verified = req.session.user?._id === result._id;
-    const username = verified ? "My" : result.username+"'s";
-    res.render('posts/index.ejs', { images, username, verified });
-  })
+
+  res.render('posts/index.ejs', { images, username, verified });
 });
 
-router.get("/:userId/new", (req, res) => { // async?
-  const uid = req.session.user?._id;
-  if (uid !== req.params.userId) { return res.redirect(`/posts/${req.params.userId}`); }
+router.get("/:username/new", (req, res) => {
+  const uploader = req.params.username;
+  const verified = req.session.user?.username === uploader;
 
-  res.render("posts/new.ejs");
+  if (verified) res.render("posts/new.ejs");
+  else res.redirect(`/users/${uploader}`);
+
 });
 
-router.get("/:userId/:imageId/edit", async (req, res) => {
-  const uid = req.session.user?._id;
-  if (uid !== req.params.userId) { return res.redirect(`/posts/${req.params.userId}/${req.params.imageId}`); }
+router.get("/:username/:imageId/edit", async (req, res) => {
+  const uploader = req.params.username;
+  const verified = req.session.user?.username === uploader;
 
-  const image = await Image.findById(req.params.imageId);
-  res.render("posts/edit.ejs", { image });
+  if (verified) {
+    const image = await Image.findById(req.params.imageId);
+    res.render("posts/edit.ejs", { image });
+  } else {
+    res.redirect(`/users/${uploader}/${req.params.imageId}`);
+  }
 });
 
-router.get("/:userId/:imageId", async (req, res) => {
-  const uid = req.session.user?._id;
-  if (uid !== req.params.userId) { return res.redirect(`/posts/${req.params.userId}/${req.params.imageId}`); }
+router.get("/:username/:imageId", async (req, res) => {
+  const uploader = req.params.username;
+  const verified = req.session.user?.username === uploader;
 
   const dbImage = await Image.findById(req.params.imageId);
-  const uploader = await User.findById(dbImage.uploader);
+  if (!dbImage) { return res.redirect(`/users/${uploader}`); 
+}
   const image = { ...dbImage._doc };
-
   image.uploadDate = formatDate(image.uploadDate);
-  image.uploaderId = image.uploader;
-  image.uploader = uploader.username;
-  res.render("posts/show.ejs", { image, verified: true });
+
+  res.render("posts/show.ejs", { image, verified });
 });
 
 //? POST routes
-router.post("/:userId/", async (req, res) => {
-  const uid = req.session.user?._id;
-  if (uid !== req.params.userId) { return res.redirect(`/posts/${req.params.userId}`); }
+router.post("/:username/", isSignedIn, async (req, res) => {
+  const uploader = req.params.username;
+  const user = await User.findOne({ username: uploader });
+  const verified = req.session.user?._id === user._id.toString();
 
   const valid = await checkImage(req.body.url);
   if (!valid) { return res.redirect(`/users/${uid}/new`); }
+  if (!verified) { return res.redirect(`/users/${uploader}`); }
 
   req.body.tags = processTags(req);
   req.body["uploader"] = req.session.user.username;
@@ -74,30 +69,35 @@ router.post("/:userId/", async (req, res) => {
   req.body.artist = req.body.artist || "Unknown";
 
   const image = await Image.create(req.body);
-  return res.redirect(`/users/${uid}/${image._id}`);
+  return res.redirect(`/users/${uploader}/${image._id}`);
 });
 
 //? PUT routes
-router.put("/:userId/:imageId", async (req, res) => {
-  const uid = req.session.user?._id;
-  if (uid !== req.params.userId) { return res.redirect(`/posts/${req.params.userId}/${req.params.imageId}`); }
+router.put("/:username/:imageId", isSignedIn, async (req, res) => {
+  const uploader = req.params.username;
+  const user = await User.findOne({ username: uploader });
+  const verified = req.session.user?._id === user._id.toString();
+
+  if (!verified) { return res.redirect(`/users/${uploader}`); }
 
   req.body.tags = processTags(req);
   await Image.findByIdAndUpdate(req.params.imageId, req.body);
-  return res.redirect(`/users/${uid}/${req.params.imageId}`);
+  return res.redirect(`/users/${uploader}/${req.params.imageId}`);
 });
 
 //? DELETE routes
-router.delete("/:userId/:imageId", async (req, res) => {
-  const uid = req.session.user?._id;
-  if (uid !== req.params.userId) { return res.redirect(`/posts/${req.params.userId}/${req.params.imageId}`); }
+router.delete("/:username/:imageId", async (req, res) => {
+  const uploader = req.params.username;
+  const user = await User.findOne({ username: uploader });
+  const verified = req.session.user?._id === user._id.toString();
+
+  if (!verified) { return res.redirect(`/users/${uploader}/${req.params.imageId}`); }
 
   await Image.findByIdAndDelete(req.params.imageId);
-  res.redirect(`/users/${uid}`);
+  res.redirect(`/users/${uploader}`);
 });
 
-
-//* helper method
+//* helper methods
 // source https://stackoverflow.com/questions/55880196/is-there-a-way-to-easily-check-if-the-image-url-is-valid-or-not
 async function checkImage(url) {
   try {
@@ -108,6 +108,17 @@ async function checkImage(url) {
   } catch (error) {
     return false;
   }
+}
+
+function formatDate(date) {
+  const year = date.getFullYear();
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0');
+  const hours = String(date.getHours()).padStart(2, '0');
+  const minutes = String(date.getMinutes()).padStart(2, '0');
+  const seconds = String(date.getSeconds()).padStart(2, '0');
+
+  return `${year}-${month}-${day} ${hours}:${minutes}:${seconds}`;
 }
 
 module.exports = router;
